afc627ef9a1d1589042b9a9dd588883a6bb5d423
NoChil1 <117416139+NoChil1@users.noreply.github.com>
2022-11-04 09:25:08 +0800

    add a file as demo1

---
+++ demo2.py
@@ -0,0 +1,14 @@
+# 姓名：zwl
+# 开发时间 2022/9/20 16:45
+n1=90
+n2=-76
+n3=0
+print(n1,type(n1))
+print(n2,type(n2))
+print(n3,type(n3))
+
+print('十进制',118)
+print('二进制',0b10101111)
+print('八进制',0o176)
+print('十六进制',0x152E)
+
